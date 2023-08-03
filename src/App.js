2cb13d39f5bdd645ef75a57a43821253255a82a6
shivuparakanatti <103122119+shivuparakanatti@users.noreply.github.com>
2023-08-03 21:18:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,10 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+          href="https://
       
         </a>
       
