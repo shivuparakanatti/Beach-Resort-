11c74a2caa38d4dfe543efb8afe06e42a7d9a944
shivuparakanatti <103122119+shivuparakanatti@users.noreply.github.com>
2023-08-05 00:15:50 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,5 +13,5 @@ root.render(
 );
 
 // If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
+// to log results 
 reportWebVitals();
